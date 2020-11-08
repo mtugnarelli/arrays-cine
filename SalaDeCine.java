bb69ed2984fe78fe74de9a8ce3820e80e87fc77b
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-11-08 17:29:29 -0300

    Interfaz

--- SalaDeCine.java
+++ SalaDeCine.java
@@ -1,4 +1,33 @@
 
 public class SalaDeCine {
 
+	/**
+	 * pre : 'fila' está en el ranto [1..5] y 'asiento' entre [1..8].
+	 * 
+	 * post: devuelve si la sala tiene libre el lugar 
+	 *       identificado por el número de fila y número de asiento.
+	 */
+	public boolean tieneLibre(int fila, int asiento) {
+		
+		return false;
+	}
+	
+	/**
+	 * pre : 'fila' está en el ranto [1..5] y 'asiento' entre [1..8].
+	 *       El asiento indicado está libre.
+	 *        
+	 * post: ocupa el lugar en la sala identificado por el número
+	 *       de fila y asiento.
+	 */
+	public void ocupar(int fila, int asiento) {
+		
+	}
+	
+	/**
+	 * post: devuelve la cantidad de lugares que están libres en la sala.
+	 */
+	public int contarLugaresLibres() {
+		
+		return 0;
+	}
 }
